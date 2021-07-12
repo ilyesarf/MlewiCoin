41ed698b0fbafdf14f6b25cbbe825936e4ad413c
An0nB0y <bam.boy2020@gmail.com>
2021-07-12 14:19:29 +0300

    Finish Coding

--- blockchain.py
+++ blockchain.py
@@ -3,9 +3,8 @@ import json
 import hashlib
 from urllib.parse import urlparse
 from time import time
-from Crypto.PublicKey import RSA
 import requests
-import uuid4
+from uuid import uuid4
 
 app = Flask(__name__)
 node_id = str(uuid4()).replace('-', '')
@@ -41,10 +40,7 @@ class Blockchain:
 		self.transaction.append(transaction)
 		return self.last_block
 
-	def last_block(self):
-
-		return self.chain[-1]
-
+	
 	def hash(self, block): #Hash_Function
 		string_object = json.dumps(block, sort_keys=True)
 		block_string = string_object.encode()
@@ -53,6 +49,12 @@ class Blockchain:
 		hex_hash = hash.hexdigest()
 
 		return hex_hash
+
+	@property
+	
+	def last_block(self):
+		return self.chain[-1]
+
 
 	def valid_chain(self, chain): #Check if the chain is valid or not
 
@@ -108,3 +110,124 @@ class Blockchain:
 
 		else:
 			raise ValueError('Invalid URL')
+
+
+	def proof_of_work(self, last_block):
+
+		last_proof = last_block['proof']
+		last_hash = self.hash(last_block)
+
+		proof = 0
+
+		while self.valid_proof(last_proof, proof, last_hash) is False:
+			proof += 1
+
+		return proof
+
+	def valid_proof(last_proof, proof, last_hash):
+
+		guess = f'{last_proof}{proof}{last_hash}'.encode()
+		hash_guess = hashlib.sha256(guess).hexdigest()
+		return hash_guess[:4] == "0000"
+
+
+blockchain = Blockchain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    # We run the proof of work algorithm to get the next proof...
+    last_block = blockchain.last_block
+    proof = blockchain.proof_of_work(last_block)
+
+    # We must receive a reward for finding the proof.
+    # The sender is "0" to signify that this node has mined a new coin.
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_identifier,
+        amount=1,
+    )
+
+    # Forge the new Block by adding it to the chain
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
+
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+
+    # Check that the required fields are in the POST'ed data
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    # Create a new Transaction
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return jsonify(response), 201
+
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+    values = request.get_json()
+
+    nodes = values.get('nodes')
+    if nodes is None:
+        return "Error: Please supply a valid list of nodes", 400
+
+    for node in nodes:
+        blockchain.register_node(node)
+
+    response = {
+        'message': 'New nodes have been added',
+        'total_nodes': list(blockchain.nodes),
+    }
+    return jsonify(response), 201
+
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+
+    if replaced:
+        response = {
+            'message': 'Our chain was replaced',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'Our chain is authoritative',
+            'chain': blockchain.chain
+        }
+
+    return jsonify(response), 200
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
+    args = parser.parse_args()
+    port = args.port
+
+    app.run(host='0.0.0.0', port=port)
